adb4b4cec8ccc4b0f42fac8e3720486b34dc749d
ayeesha234 <ayeesha.codingal@gmail.com>
2025-04-20 11:01:03 +0530

    first commit

---
+++ python/oper.py
@@ -0,0 +1,18 @@
+# Define two numbers
+num1 = 10
+num2 = 5
+
+# Perform addition and assign the result to the variable 'addition'
+addition = num1 + num2
+
+# Print the result
+print("Addition =", addition)
+
+# You can also demonstrate other operations like this:
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
